7672c7160152b6e003a7775170f95b740287723a
Noman-Nom <noman5456318@gmail.com>
2023-12-17 12:42:07 +0500

    signup page

--- auth/src/SignUp/SignUp.jsx
+++ auth/src/SignUp/SignUp.jsx
@@ -3,7 +3,36 @@ import React from 'react'
 import './SignUp.scss'
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='container'>
+
+    <div className='container__form'>
+
+        <form action="">
+
+            <div className='form__field'>
+                <label htmlFor="">Name</label>
+                <input type="text" placeholder='enter your name' />
+            </div>
+            <div className='form__field'>
+                <label htmlFor="">Email</label>
+                <input type="text" placeholder='enter your email' />
+            </div>
+            <div className='form__field'>
+                <label htmlFor="">Password</label>
+                <input type="text" placeholder='enter your password' />
+            </div>
+
+            <button>Signup</button>
+
+            <div>
+                <Link to='/signup'> <p>Login</p></Link>
+            </div>
+
+        </form>
+
+
+    </div>
+</div>
   )
 }
 
