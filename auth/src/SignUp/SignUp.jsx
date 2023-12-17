766b13d0d924fe83f6b91b44edd12c937936bee1
Noman-Nom <noman5456318@gmail.com>
2023-12-17 15:40:13 +0500

    validation for signup page

--- auth/src/SignUp/SignUp.jsx
+++ auth/src/SignUp/SignUp.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+
+
+
+import Validation from '../SignupValidation'
 
 import './SignUp.scss'
 import { Link } from 'react-router-dom'
 const SignUp = () => {
+
+    const [values, setValues] = useState({
+        name:"",
+        email:"",
+        password:""
+    
+    })
+
+    const handleChange  = (e)=>{
+
+        setValues((prev)=>({...prev , [e.target.name]: e.target.value }))
+    }
+
+    console.log(values)
   return (
     <div className='container'>
 
@@ -12,15 +31,15 @@ const SignUp = () => {
 
             <div className='form__field'>
                 <label htmlFor="">Name</label>
-                <input type="text" placeholder='enter your name' />
+                <input  onChange={handleChange} name='name' type="text" placeholder='enter your name' />
             </div>
             <div className='form__field'>
-                <label htmlFor="">Email</label>
-                <input type="text" placeholder='enter your email' />
+                <label name='email' htmlFor="">Email</label>
+                <input  onChange={handleChange} type="email" placeholder='enter your email' />
             </div>
             <div className='form__field'>
                 <label htmlFor="">Password</label>
-                <input type="text" placeholder='enter your password' />
+                <input  onChange={handleChange} name='password' type="password" placeholder='enter your password' />
             </div>
 
             <button>Signup</button>
---
+++ auth/src/SignupValidation.js
@@ -0,0 +1,34 @@
+const Validation = (values) => {
+  // console.log(values)
+  let error = {};
+
+  const email_pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  const password_pattern = /^(?=.*[a-zA-Z]).{6,}$/;
+
+  if (values.name === "") {
+    error.name = "name Should not be empty";
+  
+  } else {
+    error.name = "";
+  }
+
+  
+  if (values.email === "") {
+    error.email = "Email Should not be empty";
+  } else if (!email_pattern.test(values.email)) {
+    error.email = "Email Didn't Match";
+  } else {
+    error.email = "";
+  }
+  if (values.password === "") {
+    error.password = "password Should not be empty";
+  } else if (!password_pattern.test(values.password)) {
+    error.password = " Password Didn't Match";
+  } else {
+    error.password = "";
+  }
+
+  return error;
+};
+
+export default Validation;
