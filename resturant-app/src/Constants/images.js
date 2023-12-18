6ccc6f791c3b7b63dd381b3c5d1ef9264cf64cb8
Noman-Nom <noman5456318@gmail.com>
2023-12-18 13:01:27 +0500

    video page completed

--- resturant-app/src/Constants/images.js
+++ resturant-app/src/Constants/images.js
@@ -2,11 +2,14 @@ import logo from '../assets/nomLogo2.png'
 
 import banner from '../assets/baner.png'
 import video1 from '../assets/v1.mp4'
-import video1 from '../assets/v1.mp4'
+
 import video2 from '../assets/v2.mp4'
 import video3 from '../assets/v3.mp4'
 
 export default {
     logo,
-    banner
+    banner,
+    video1,
+    video2,
+    video3,
 }
---
+++ resturant-app/src/Pages/PromoPage/PromoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PromoPage.scss'
+import Promo from '../../Components/Promo/Promo'
+const PromoPage = () => {
+  return (
+    <div>
+      <Promo/>
+    </div>
+  )
+}
+
+export default PromoPage
