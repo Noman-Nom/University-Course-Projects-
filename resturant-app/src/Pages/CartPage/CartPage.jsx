5188f4e0e9267e20d8011a91db4f470996f007e3
Noman-Nom <noman5456318@gmail.com>
2024-01-01 15:04:55 +0500

    cart page working

--- resturant-app/src/Pages/CartPage/CartPage.jsx
+++ resturant-app/src/Pages/CartPage/CartPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Cart from '../../Components/Cart/Cart'
+import Navbar from '../../Components/Navbar/Navbar'
 
 const CartPage = () => {
   return (
     <div>
+        <div className='top'>
+
+<Navbar className="navbar" />
+</div>
 
         <Cart/>
     </div>
