4bad486ea7612422028f0309dbb3e12a04363ede
Noman-Nom <noman5456318@gmail.com>
2024-01-01 15:32:09 +0500

    all food page

--- resturant-app/src/Pages/AllFoodPage/AllFoodPage.jsx
+++ resturant-app/src/Pages/AllFoodPage/AllFoodPage.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import AllFood from '../../Components/AllFood/AllFood'
 import Navbar from '../../Components/Navbar/Navbar'
-import './AllFoodPage.scss'
 
 const AllFoodPage  =  () => {
   return (
-    <div className='container'>
+    <div>
+    <div className='top'>
 
-      <div className='top'>
-      <Navbar/>
+<Navbar className="navbar" />
+</div>
 
-      </div>
-
-      <div className='bottom'>
-
-      <AllFood/>
-      </div>
-    </div>
+    <AllFood/>
+</div>
   )
 }
 
