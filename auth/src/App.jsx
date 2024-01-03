333d006e45c5422fb94bc596076205c1c1e3b9cc
Noman-Nom <noman5456318@gmail.com>
2024-01-04 01:22:49 +0500

    auth backend

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './Login/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './SignUp/SignUp'
+import Home from './Home/Home'
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login/>}/>
-          <Route path='signup' element={<SignUp/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/home' element={<Home/>}/>
 
         </Routes>
       </BrowserRouter>
